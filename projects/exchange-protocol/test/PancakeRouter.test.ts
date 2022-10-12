f6b6fc7fa7e1b5c53f3981fa2beb484a24294c73
Nakarin Kakanumporn <belizwp@live.com>
2022-10-12 14:39:28 +0700

    setup tests with ethers

--- projects/exchange-protocol/test/PancakeRouter.test.ts
+++ projects/exchange-protocol/test/PancakeRouter.test.ts
@@ -1,122 +1,135 @@
 /* eslint-disable */
 
+import { assert } from "chai";
 import { formatEther, formatUnits, parseEther } from "ethers/lib/utils";
 import { artifacts, contract } from "hardhat";
-import { assert, expect } from "chai";
-import { BN, constants, expectEvent, expectRevert, time } from "@openzeppelin/test-helpers";
-import { BigNumber } from "ethers";
+// import { assert, expect } from "chai";
+// import { BN, constants, expectEvent, expectRevert, time } from "@openzeppelin/test-helpers";
+// import { BigNumber } from "ethers";
 
-const MockERC20 = artifacts.require("./utils/MockERC20.sol");
-const PancakeFactory = artifacts.require("./PancakeFactory.sol");
+const { ethers } = require("hardhat");
+
+
+// const MockERC20 = artifacts.require("./utils/MockERC20.sol");
+// const PancakeFactory = artifacts.require("./PancakeFactory.sol");
 const PancakePair = artifacts.require("./PancakePair.sol");
-const PancakeRouter = artifacts.require("./PancakeRouter.sol");
-const WBNB = artifacts.require("./WBNB.sol");
-const MinimalForwarder = artifacts.require("@openzeppelin/contracts/metatx/MinimalForwarder.sol");
+// const PancakeRouter = artifacts.require("./PancakeRouter.sol");
+// const WBNB = artifacts.require("./WBNB.sol");
+// const MinimalForwarder = artifacts.require("@openzeppelin/contracts/metatx/MinimalForwarder.sol");
 
-contract("PancakeRouter", ([alice, bob, carol, david, erin]) => {
-  let pancakeFactory: any;
+import { PancakeFactory } from '../typechain/PancakeFactory'
+
+async function deploy(name, ...params) {
+  const Contract = await ethers.getContractFactory(name);
+  return await Contract.deploy(...params).then((f) => f.deployed());
+}
+
+describe("PancakeRouter", function () {
+  let pancakeFactory: PancakeFactory;
   let wrappedBNB: any;
-  let minimalForwarder: any;
+  let forwarder: any;
   let pancakeRouter: any;
   let pairAB;
   let tokenA;
   let tokenB;
 
-  let owner = alice;
-  let feeManager = bob;
-  let user = carol;
-  let relayer = david;
-  let unknownUser = erin;
+  let owner;
+  let feeManager;
+  let user;
+  let relayer;
+  let unknownUser;
 
   before(async () => {
-    // Deploy Factory
-    pancakeFactory = await PancakeFactory.new(owner, { from: owner });
+    const accounts = await ethers.getSigners();
 
-    // Deploy Wrapped BNB
-    wrappedBNB = await WBNB.new({ from: owner });
+    owner = accounts[0];
+    feeManager = accounts[1];
+    user = accounts[2];
+    relayer = accounts[3];
+    unknownUser = accounts[4];
 
-    // Deploy MinimalForwarder
-    minimalForwarder = await MinimalForwarder.new({ from: owner });
-
-    // Deploy Router
-    pancakeRouter = await PancakeRouter.new(
-      pancakeFactory.address,   // Factory
-      wrappedBNB.address,       // Wrapped ETH or BNB
-      feeManager,               // FeeManager
-      minimalForwarder.address, // Forwarder
-      { from: owner }
-    );
-
+    pancakeFactory = await deploy("PancakeFactory", owner.address);
     console.log("PancakeFactory deployed to:", pancakeFactory.address);
+
+    wrappedBNB = await deploy("WBNB");
+    console.log("WBNB deployed to:", wrappedBNB.address);
+
+    forwarder = await deploy("MinimalForwarder");
+    console.log("forwarder deployed to:", forwarder.address);
+
+    pancakeRouter = await deploy(
+      "PancakeRouter",
+      pancakeFactory.address,
+      wrappedBNB.address,
+      feeManager.address,
+      forwarder.address,
+    );
     console.log("PancakeRouter deployed to:", pancakeRouter.address);
 
-    // Deploy ERC20s
-    tokenA = await MockERC20.new("Token A", "TA", parseEther("10000000"), { from: owner });
-    tokenB = await MockERC20.new("Token B", "TB", parseEther("10000000"), { from: owner });
+    tokenA = await deploy("MockERC20", "Token A", "TKA", parseEther("1000000"));
+    console.log("Token A deployed to:", tokenA.address);
+    tokenB = await deploy("MockERC20", "Token B", "TKB", parseEther("1000000"));
+    console.log("Token B deployed to:", tokenB.address);
 
-    console.log("TokenA deployed to:", tokenA.address);
-    console.log("TokenB deployed to:", tokenB.address);
-
-    // Create Pair of tokenA and tokenB
-    let result = await pancakeFactory.createPair(tokenA.address, wrappedBNB.address, { from: owner });
-    pairAB = await PancakePair.at(result.logs[0].args[2]);
+    let result = await pancakeFactory.connect(owner).createPair(tokenA.address, wrappedBNB.address).then((tx) => tx.wait());
+    console.log("createPair result:", result.events[0].args.pair);
+    const pairABAddress = result.events[0].args.pair
+    pairAB = await PancakePair.at(pairABAddress);
     assert.equal(String(await pairAB.totalSupply()), parseEther("0").toString());
-    console.log("PairAB created at:", pairAB.address);
-
-    // Mint tokenA and tokenB for Owner
-    await tokenA.mintTokens(parseEther("2000000"), { from: owner });
-    await tokenB.mintTokens(parseEther("2000000"), { from: owner });
 
     // Provide liquidity to pairAB by Owner
-    await tokenA.approve(pancakeRouter.address, constants.MAX_UINT256, {
-      from: owner,
-    });
-    await tokenB.approve(pancakeRouter.address, constants.MAX_UINT256, {
-      from: owner,
-    });
-    const deadline = new BN(await time.latest()).add(new BN("100"));
-    await pancakeRouter.addLiquidity(
+    await tokenA.connect(owner).approve(pancakeRouter.address, ethers.constants.MaxUint256);
+    await tokenB.connect(owner).approve(pancakeRouter.address, ethers.constants.MaxUint256);
+
+    const deadline = ethers.constants.MaxUint256;
+
+    await pancakeRouter.connect(owner).addLiquidity(
       tokenA.address,
       tokenB.address,
       parseEther("1000000"), // 1M token A
       parseEther("1000000"), // 1M token B
       parseEther("1000000"),
       parseEther("1000000"),
-      owner,
+      owner.address,
       deadline,
-      { from: owner }
     );
   });
 
-  describe("User swap token A for token B", () => {
-    it("exact in fail if sender is not gasless role", async () => {
-      // Setup token A for User
-      await tokenA.mintTokens(parseEther("100"), { from: unknownUser });
-      await tokenA.approve(pancakeRouter.address, constants.MAX_UINT256, {
-        from: unknownUser,
-      });
+  describe("Deploy", () => {
+    it("deploy something", async () => {
 
-      console.log("Token A before balance:", formatEther(BigNumber.from((await tokenA.balanceOf(unknownUser)).toString())));
-      console.log("Token B before balance:", formatEther(BigNumber.from((await tokenB.balanceOf(unknownUser)).toString())));
+    })
+  })
 
-      const deadline = new BN(await time.latest()).add(new BN("100"));
-      const task = pancakeRouter.swapExactTokensForTokensWithGasless(
-        parseEther("90"),  // Token A
-        parseEther("0.9"), // Token B
-        parseEther("10"),  // Token A Fee 
-        [tokenA.address, tokenB.address],
-        unknownUser, // To
-        deadline,
-        { from: unknownUser }
-      );
+  // describe("User swap token A for token B", () => {
+  //   it("exact in fail if sender is not gasless role", async () => {
+  //     // Setup token A for User
+  //     await tokenA.mintTokens(parseEther("100"), { from: unknownUser });
+  //     await tokenA.approve(pancakeRouter.address, constants.MAX_UINT256, {
+  //       from: unknownUser,
+  //     });
 
-      console.log("Token A after balance:", formatEther(BigNumber.from((await tokenA.balanceOf(unknownUser)).toString())));
-      console.log("Token B after balance:", formatEther(BigNumber.from((await tokenB.balanceOf(unknownUser)).toString())));
+  //     console.log("Token A before balance:", formatEther(BigNumber.from((await tokenA.balanceOf(unknownUser)).toString())));
+  //     console.log("Token B before balance:", formatEther(BigNumber.from((await tokenB.balanceOf(unknownUser)).toString())));
 
-      await expectRevert(
-        task,
-        "PancakeRouter: must have gasless role"
-      );
-    });
-  });
+  //     const deadline = new BN(await time.latest()).add(new BN("100"));
+  //     const task = pancakeRouter.swapExactTokensForTokensWithGasless(
+  //       parseEther("90"),  // Token A
+  //       parseEther("0.9"), // Token B
+  //       parseEther("10"),  // Token A Fee 
+  //       [tokenA.address, tokenB.address],
+  //       unknownUser, // To
+  //       deadline,
+  //       { from: unknownUser }
+  //     );
+
+  //     console.log("Token A after balance:", formatEther(BigNumber.from((await tokenA.balanceOf(unknownUser)).toString())));
+  //     console.log("Token B after balance:", formatEther(BigNumber.from((await tokenB.balanceOf(unknownUser)).toString())));
+
+  //     await expectRevert(
+  //       task,
+  //       "PancakeRouter: must have gasless role"
+  //     );
+  //   });
+  // });
 });
